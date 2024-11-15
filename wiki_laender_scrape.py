3d067a4cf7d914ab05cdca31cc745b03efe69f71
CaGListRo <paco.nyns@gmail.com>
2024-11-15 11:59:34 +0100

    Added type hints.

--- wiki_laender_scrape.py
+++ wiki_laender_scrape.py
@@ -3,12 +3,14 @@ import requests
 import pandas as pd
 import numpy as np
 
-url = 'https://de.wikipedia.org/wiki/Liste_der_Staaten_der_Erde'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-country_table = soup.find('table')
-head_row = country_table.find('tr')
-chart_titles = []
+
+
+url: str = 'https://de.wikipedia.org/wiki/Liste_der_Staaten_der_Erde'
+response: requests = requests.get(url)
+soup: BeautifulSoup = BeautifulSoup(response.content, 'html.parser')
+country_table: str = soup.find('table')
+head_row: str = country_table.find('tr')
+chart_titles: list[str] = []
 for i, tit in enumerate(head_row):
     if i % 2 != 0:
         title = tit.text.strip()
@@ -18,16 +20,17 @@ for i, tit in enumerate(head_row):
             title = 'Flächein km²'
         chart_titles.append(title)
 
-df = pd.DataFrame(columns=chart_titles)
+df: pd.DataFrame = pd.DataFrame(columns=chart_titles)
 print(len(chart_titles))
-column_data = country_table.find_all('tr')
-add_count, rel_count = 0, 0
+column_data: str = country_table.find_all('tr')
+add_count: int = 0
+rel_count: int = 0
 for row in column_data[3:]:
-    row_data = row.find_all('td')
-    individual_row_data = []
+    row_data: str = row.find_all('td')
+    individual_row_data: list[str] = []
     print(row_data)
     for data in row_data[:len(chart_titles)]:  # Begrenze die Anzahl der Elemente in row_data auf die Anzahl der Spalten in chart_titles
-        temp_row_data = data.text.strip()
+        temp_row_data: list[str] = data.text.strip()
         if temp_row_data == '':
             temp_row_data = np.nan  # Leeres Feld wird mit "NaN" gefüllt
         individual_row_data.append(temp_row_data)
